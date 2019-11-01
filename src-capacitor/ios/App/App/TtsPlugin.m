343b68f7f2ba32ad74ee188d0b74b97fcb092192
PierreGreen <pierreguthauser@gmail.com>
2019-11-01 17:29:04 +0100

    Added IOS target / TtsPlugin

---
+++ src-capacitor/ios/App/App/TtsPlugin.m
@@ -0,0 +1,8 @@
+//
+//  TtsPlugin.m
+//  App
+//
+//  Created by Pierre GUTHAUSER on 11/1/19.
+//
+
+#import <Foundation/Foundation.h>
---
+++ src-capacitor/ios/App/App/TtsPlugin.swift
@@ -0,0 +1,8 @@
+//
+//  TtsPlugin.swift
+//  App
+//
+//  Created by Pierre GUTHAUSER on 11/1/19.
+//
+
+import Foundation
