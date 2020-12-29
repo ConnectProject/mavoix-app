c1bcdd1d03589c19f6371c52f848c1bfe0904de8
r03ert0 <rto@braincatalogue.org>
2020-12-29 18:50:40 +0100

    dim down tab colours

--- src/utils/randomHex.js
+++ src/utils/randomHex.js
@@ -1,6 +1,21 @@
 /**
  * Generate a random 16 bytes hex string
  */
-export default () => (
-  `#${(Math.random() * 0xFFFFFF << 0).toString(16)}`
-)
+export default () => {
+  const h = `#${(Math.random() * 0xFFFFFF << 0).toString(16)}`
+
+  // change colour saturation, make it darker
+  const rgb = [
+    `0x${h[1] + h[2]}` | 0,
+    `0x${h[3] + h[4]}` | 0,
+    `0x${h[5] + h[6]}` | 0
+  ]
+
+  const h2 = `#${rgb.map((o) => {
+    const x = (60 + o * 0.5) | 0
+    const s = ('00' + x.toString(16)).slice(-2)
+    return s
+  }).join('')}`
+
+  return h2
+}
