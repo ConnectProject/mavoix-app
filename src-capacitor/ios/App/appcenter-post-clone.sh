3692058545e9b18811985886521a8a6157ab4132
Nicolas <nicolas@Nicolass-MacBook-Pro.local>
2020-12-31 05:10:07 -0800

    use quasar build in app center

--- src-capacitor/ios/App/appcenter-post-clone.sh
+++ src-capacitor/ios/App/appcenter-post-clone.sh
@@ -6,5 +6,5 @@ npm install -g @quasar/cli
 perl -lpe 's|(?<=PARSE_URL=).*|https://ghfc17.hosting.pasteur.fr/parse|' .env.example > .env
 echo "Generated .env file:"
 cat .env
-# quasar build -m capacitor -T ios
-(quasar dev -m capacitor -T ios &) | sed -e '/Sync finished/q'; pkill -f quasar
+quasar build -m capacitor -T ios
+# (quasar dev -m capacitor -T ios &) | sed -e '/Sync finished/q'; pkill -f quasar
