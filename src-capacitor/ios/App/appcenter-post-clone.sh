9f55a3d980d7b6f0e9612ff847620350e74b2607
Nicolas <nicolas@Nicolass-MacBook-Pro.local>
2020-12-31 02:44:49 -0800

    use quasar dev in app center to try to avoid building twice

--- src-capacitor/ios/App/appcenter-post-clone.sh
+++ src-capacitor/ios/App/appcenter-post-clone.sh
@@ -3,4 +3,5 @@
 cd ../../..
 npm install
 npm install -g @quasar/cli
-quasar build -m capacitor -T ios || true
+# quasar build -m capacitor -T ios
+(quasar dev -m capacitor -T ios &) | sed -e '/Sync finished/q'; pkill -f quasar
