7581c97b92b756ba297a4d5d6ec638451c994059
nicolas <ntraut9@gmail.com>
2024-02-12 18:59:37 +0100

    use direct speech synthesis api without capacitor plugin

--- src/store/tts/actions.js
+++ src/store/tts/actions.js
@@ -1,19 +1,45 @@
-import { Plugins } from '@capacitor/core'
+let languagesObject
 
-const { TtsPlugin } = Plugins
+function loadVoices ({ commit }) {
+  try {
+    languagesObject = {}
+    console.log('will get voices')
+    const voices = speechSynthesis.getVoices()
+    console.log(`${voices.length} voices loaded`)
+
+    // Organize voices by language
+    voices.forEach((voice) => {
+      const { lang } = voice
+      if (!languagesObject[lang]) {
+        languagesObject[lang] = []
+      }
+      languagesObject[lang].push(voice)
+    })
+
+    if ('en-US' in languagesObject) {
+      commit('ttsEnabled')
+    }
+  } catch (err) {
+    commit('setError', err)
+  }
+}
 
 /**
  * Init capacitor's text to speech plugin
  * @param {Context} context
  */
 export const init = ({ commit }) => {
-  TtsPlugin.load({ locale: 'en_US', voice: 4 })
-    .then(() => {
-      commit('ttsEnabled')
-    })
-    .catch((err) => {
-      commit('setError', err)
-    })
+  try {
+    // Execute loadVoices.
+    loadVoices({ commit })
+
+    // Chrome loads voices asynchronously.
+    speechSynthesis.onvoiceschanged = function (e) {
+      loadVoices({ commit })
+    }
+  } catch (err) {
+    commit('setError', err)
+  }
 }
 
 /**
@@ -21,24 +47,27 @@ export const init = ({ commit }) => {
  * @param {Context} context
  * @param {String} text the phrase to speek
  */
-export const speak = ({ commit, rootState, getters: { playing, tts } }, text) => {
-  if (tts) {
-    commit('setPlaying', true)
-    console.log(rootState.dropZone.tab.language)
-    console.log(rootState.dropZone.tab.speed)
-    TtsPlugin.speak({ text: text, locale: rootState.dropZone.tab.language, rate: '' + rootState.dropZone.tab.speed })
-      .then(res => {
-        let rate = 1
-        if (rootState.dropZone.tab.speed) {
-          rate = rootState.dropZone.tab.speed
-        }
-        let timer = text.length * 50 / rate
-        setTimeout(() => {
-          commit('setPlaying', false)
-        }, timer)
-      })
-      .catch((err) => {
-        commit('setError', err)
-      })
+export const speak = ({ commit, rootState, getters: { tts } }, text) => {
+  try {
+    if (!tts) {
+      console.log('trying to load voices')
+      loadVoices({ commit })
+    }
+    if (tts) {
+      commit('setPlaying', true)
+
+      const utterance = new SpeechSynthesisUtterance(text)
+      utterance.rate = parseFloat(rootState.dropZone.tab.speed) || 1
+      const locale = rootState.dropZone.tab.language.replace('_', '-')
+      utterance.voice = languagesObject[locale][0]
+      console.log(utterance)
+
+      utterance.onend = function () {
+        commit('setPlaying', false)
+      }
+      speechSynthesis.speak(utterance)
+    }
+  } catch (err) {
+    commit('setError', err)
   }
 }
